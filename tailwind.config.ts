3965f923a2c8bddf53671e3d5e0e355605a38a11
zimnickico <hi+github@krzysztofzimnicki.com>
2025-02-19 15:10:28 +0100

    initialize
    + add posthog
    + add auth
    + start ui

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,35 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./ui/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#000000",
+        inverted: "#ffffff",
+        secondary: "#a0a0a0",
+      },
+      height: {
+        button: "40px",
+      },
+      borderRadius: {
+        button: "15px",
+      },
+      padding: {
+        button: "15px 20px",
+        safe: "30px",
+        "extra-safe": "80px",
+      },
+      fontWeight: {
+        heading: "600",
+      },
+      fontSize: {
+        heading: "30px",
+      },
+      lineHeight: {
+        heading: "35px",
       },
     },
   },
